799ec5cf3936abe805f14f43fa9a9151998fedad
Neeraj Yadav <neeraj0yadav0@gmail.com>
2022-05-29 22:29:22 +0530

    header aviator

--- src/Pages/Header.js
+++ src/Pages/Header.js
@@ -2,6 +2,12 @@ import React from "react";
 
 import SearchIcon from "@material-ui/icons/Search";
 import "./Header.css";
+import HeaderOption from "./HeaderOption";
+import HomeIcon from "@material-ui/icons/Home";
+import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
+import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
+import ChatIcon from "@material-ui/icons/Chat";
+import NotificationsIcon from "@material-ui/icons/Notifications";
 
 function Header() {
   return (
@@ -13,11 +19,22 @@ function Header() {
           alt="Error"
         />
         <div className="header_search">
+          {/* search */}
           <SearchIcon />
           <input type="text" />
         </div>
       </div>
-      {/* search */}
+      <div className="header_right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+        <HeaderOption
+          avatar="https://w0.peakpx.com/wallpaper/212/254/HD-wallpaper-alone-boy-no-face-viral-pic-amit-raftaar-sexy-boy.jpg"
+          title="Me"
+        />
+      </div>
       {/* icons */}
       {/* right */}
     </div>
